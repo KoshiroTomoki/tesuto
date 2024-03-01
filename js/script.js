a747786b96b1b0cc4ebe5ffe92af48066b49be90
KoshiroTomoki <149355180+KoshiroTomoki@users.noreply.github.com>
2024-03-01 17:13:05 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -30,3 +30,24 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+{
+    const menuItems = document.querySelectorAll('.chargeButton');
+    const contents = document.querySelectorAll('.hiddenContent');
+
+    menuItems.forEach(clickedItem => {
+        clickedItem.addEventListener('click', e => {
+            e.preventDefault();
+
+            menuItems.forEach(item => {
+                item.classList.remove('active');
+            });
+            clickedItem.classList.add('active');
+
+            contents.forEach(content => {
+                content.classList.remove('active');
+            });
+            document.getElementById(clickedItem.dataset.id).classList.add('active');
+        });
+    });
+}
